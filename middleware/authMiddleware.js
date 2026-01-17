de0aa01d417d9580e6dbdd3d0ed60f26c4122b25
oussasz <brahimi.oussamaa@gmail.com>
2026-01-17 21:06:24 +0100

    Restore auth and multer middleware for routes

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+export function authenticateUser(req, res, next) {
+  const JWT_SECRET = process.env.JWT_SECRET;
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: "Authentication required - No token provided" });
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      console.error("Error authenticating user:", error);
+      return res.status(401).json({ error: "Invalid token" });
+    }
+    if (error.name === "TokenExpiredError") {
+      console.error("Error authenticating user:", error);
+      return res.status(403).json({ error: "Token expired" });
+    }
+    console.error("Error authenticating user:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
---
+++ middleware/multerMiddleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+import path from "path";
+
+const filename = function (req, file) {
+  const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+  return file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname);
+};
+
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, callback) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return callback(new Error("Only JPG, PNG, or WEBP files are allowed."));
+    }
+    callback(null, true);
+  },
+  filename: filename,
+});
